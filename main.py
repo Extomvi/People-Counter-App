e8275407e4820f7db2f9155937098442df69b6dd
Extomvi <exceltadedokun@gmail.com>
2020-05-25 15:32:09 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -84,15 +84,39 @@ def infer_on_stream(args, client):
     :param client: MQTT client
     :return: None
     """
+    total_count = 0
+    last_count = 0
+    start_time = 0
+    request_id = 0
     # Initialise the class
     infer_network = Network()
     # Set Probability threshold for detections
     prob_threshold = args.prob_threshold
 
     ### TODO: Load the model through `infer_network` ###
-    
+    infer_network.load_model(args.model, args.device)
+    net_input_shape = infer_network.get_input_shape()
     ### TODO: Handle the input stream ###
+    if args.input == "CAM":
+        input_stream = 0
+    else:
+        input_stream = args.input
+        assert os.path.isfile(args.input), "Specified input file doesn't exist"
 
+    try:
+        cap = cv2.VideoCapture(args.input)
+    except FileNotFoundError:
+        print("Cannot locate video file: " + args.input)
+    except Exception as e:
+        print("Something went wrong with the video file: ", e)
+        
+    if input_stream:
+        cap.open(args.input)
+    if not cap.isOpened():
+        log.error("Can't open video source")
+        
+    width = int(cap.get(3))
+    height = int(cap.get(4))
     ### TODO: Loop until stream is over ###
 
         ### TODO: Read from the video capture ###
@@ -115,7 +139,60 @@ def infer_on_stream(args, client):
         ### TODO: Send the frame to the FFMPEG server ###
 
         ### TODO: Write an output image if `single_image_mode` ###
+    while cap.isOpened():
+        # Read the next frame
+        flag, frame = cap.read()
+        if not flag:
+            break
+        key_pressed = cv2.waitKey(60)
+        pro_frame = cv2.resize(frame, (net_input_shape[3], net_input_shape[2]))
+        pro_frame = pro_frame.transpose((2,0,1))
+        pro_frame = pro_frame.reshape(1, *pro_frame.shape)
 
+        inf_start = time.time()
+        infer_network.async_inference(pro_frame)
+            
+        if infer_network.wait() == 0:
+            det_time = time.time() - inf_start
+            result = infer_network.get_output()
+                
+            current_count = 0
+            for box in result[0][0]: 
+                # Draw bounding box for object when it's probability is more than
+                #  the specified threshold
+                conf = box[2]
+                if box[2] > prob_threshold:
+                    xmin = int(box[3] * width)
+                    ymin = int(box[4] * height)
+                    xmax = int(box[5] * width)
+                    ymax = int(box[6] * height)
+                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (225, 225, 225), 1)
+                    current_count = current_count + 1
+            inf_time_message = "Inference time: {:.3f}ms" \
+                .format(det_time * 1000)
+            cv2.putText(frame, inf_time_message, (15, 15),
+                            cv2.FONT_HERSHEY_COMPLEX, 0.5, (200, 10, 10), 1)
+            if current_count > last_count:
+                start_time = time.time()
+                total_count = total_count + current_count - last_count
+                client.publish("person", json.dumps({"total": total_count}))
+            if current_count < last_count:
+                duration = int(time.time() - start_time)
+                client.publish("person/duration",
+                               json.dumps({"duration": duration}))
+            client.publish("person", json.dumps({"count": current_count}))
+            last_count = current_count
+
+            if key_pressed == 27:
+                break
+        sys.stdout.buffer.write(frame)
+        sys.stdout.flush()
+        
+    cap.release()
+    cv2.destroyAllWindows()
+    client.disconnect()
+    infer_network.clear()
+        
 
 def main():
     """
@@ -133,4 +210,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+    exit(0)
 
